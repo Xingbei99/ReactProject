c3dafa5f233c71f8dc91d907be632a9405a5a5de
Xingbei99 <xingbei.cao@vanderbilt.edu>
2020-06-23 10:24:25 -0500

    First

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+
+// Heading is the child of App
+function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
